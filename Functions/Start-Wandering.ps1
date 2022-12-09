a855cbfe7a48e2be6589f2074edd47f96bfd8147
ppacevnet <91396775+ppacevnet@users.noreply.github.com>
2022-12-09 16:15:34 -0600

    added random type

--- Functions/Start-Wandering.ps1
+++ Functions/Start-Wandering.ps1
@@ -12,44 +12,65 @@
 .Functionality
 #>
 Function Start-Wandering {
+    param (
+        [Parameter()][String]$Type = "Laser"
+    )
+
     $WanderingMaxWidth = ((Get-Host).ui.rawui.windowsize.width) - 3
-    $WanderingWandererLocation = 0
-    $WanderingWandererDirection = "right"
-    $WanderingWandererRight = "\"
-    $WanderingWandererLeft = "/"
-    $WanderingWanderer = $WanderingWandererRight
     $WanderingWandererColors = @(0..255)
-    $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
 
-    #Main loop
-    While ($True) {
-        #Moving the wanderer, turning him when he hits the end.
-        If ($WanderingWandererDirection -eq "right") {
-            If ($WanderingWandererLocation -eq ($WanderingMaxWidth + 1)) {
-                $WanderingWandererDirection = "left"
-                $WanderingWanderer = $WanderingWandererLeft
-                $WanderingWandererLocation--
-                $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
+    If ($Type -eq "laser") {
+        $WanderingWandererLocation = 0
+        $WanderingWandererDirection = "right"
+        $WanderingWandererRight = "\"
+        $WanderingWandererLeft = "/"
+        $WanderingWanderer = $WanderingWandererRight
+        $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
+
+        #Main loop
+        While ($True) {
+            #Moving the wanderer, turning him when he hits the end.
+            If ($WanderingWandererDirection -eq "right") {
+                If ($WanderingWandererLocation -eq ($WanderingMaxWidth + 1)) {
+                    $WanderingWandererDirection = "left"
+                    $WanderingWanderer = $WanderingWandererLeft
+                    $WanderingWandererLocation--
+                    $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
+                } Else {
+                    $WanderingWandererLocation++
+                }
             } Else {
-                $WanderingWandererLocation++
+                If ($WanderingWandererLocation -eq 0) {
+                    $WanderingWandererDirection = "right"
+                    $WanderingWanderer = $WanderingWandererRight
+                    $WanderingWandererLocation++
+                    $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
+                } Else {
+                    $WanderingWandererLocation--
+                }
             }
-        } Else {
-            If ($WanderingWandererLocation -eq 0) {
-                $WanderingWandererDirection = "right"
-                $WanderingWanderer = $WanderingWandererRight
-                $WanderingWandererLocation++
-                $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
-            } Else {
-                $WanderingWandererLocation--
+            $WanderingOutputFiller = ""
+
+            For ($I = 0; $I -lt $WanderingWandererLocation; $I++) {
+                $WanderingOutputFiller += " "
             }
-        }
-        $WanderingOutputFiller = ""
 
-        For ($I = 0; $I -lt $WanderingWandererLocation; $I++) {
-            $WanderingOutputFiller += " "
+            $WanderingOutputString = $WanderingOutputFiller, "`e[38;5;", $WanderingWandererCurrentColor, "m", $WanderingWanderer, "`e[0m" | Join-String
+            $WanderingOutputString
         }
+    } ElseIf ($Type -eq "Random") {
+        $WanderingWanderer = "●"
+        While ($True) {
+            $WanderingWandererLocation = (0..$WanderingMaxWidth) | Get-Random
+            $WanderingWandererCurrentColor = $WanderingWandererColors | Get-Random
 
-        $WanderingOutputString = $WanderingOutputFiller, "`e[38;5;", $WanderingWandererCurrentColor, "m", $WanderingWanderer, "`e[0m" | Join-String
-        $WanderingOutputString
+            $WanderingOutputFiller = ""
+            For ($I = 0; $I -lt $WanderingWandererLocation; $I++) {
+                $WanderingOutputFiller += " "
+            }
+
+            $WanderingOutputString = $WanderingOutputFiller, "`e[38;5;", $WanderingWandererCurrentColor, "m", $WanderingWanderer, "`e[0m" | Join-String
+            $WanderingOutputString
+        }
     }
 }
